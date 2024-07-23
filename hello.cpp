5b388c47cbac7ef2836501a7e1467d59d66637d8
jha-niraj <jhaniraj45@gmail.com>
2024-07-23 10:23:05 +0530

    Added the vote syste in hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World" << endl;
+	cout << "Hello World" << endl;
+
+	int age;
+	cout << "Enter your age: ";
+	cin >> age;
+
+	if(age >= 18) {
+		cout << "Hey, you are eligible to vote" << endl;
+	} else {
+		cout << "You are not allowed to vote." << endl;
+	}
 }
